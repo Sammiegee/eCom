897f9c0f1ba0db2207e55f53dd53e388fcbfaab5
sammiegee <samuelolutola@gmail.com>
2022-05-07 08:39:30 +0100

    created a new app called admindashboard

--- eCommerce/urls.py
+++ eCommerce/urls.py
@@ -20,6 +20,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('samshop.urls')),
+    path('', include('admindashboard.urls')),
     path('accounts/', include("django.contrib.auth.urls")),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
